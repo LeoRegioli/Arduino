c2cd242e77d2b750ea100f9e6e1ccab866e158b5
Leonardo Regioli <henrique_11@msn.com>
2017-10-14 14:35:49 -0300

    Arduino LDR

---
+++ app.ino
@@ -0,0 +1,38 @@
+int led = 13,
+    LDR = 0,
+    valorObtido = 0;
+bool a = false;
+long auxTime = 0;
+/*-----------------------------------------------*/
+
+void setup()
+{
+    pinMode(led, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    valorObtido = analogRead(LDR);
+    if (valorObtido >= 690 && a == false)
+    {
+        digitalWrite(led, HIGH);
+        Serial.print(valorObtido);
+        Serial.println(" - Led ligado!");
+        a = true;
+    }
+
+    if (valorObtido <= 689 && a == true)
+    {
+        digitalWrite(led, LOW);
+        Serial.print(valorObtido);
+        Serial.println(" - Led desligado!");
+        a = false;
+    }
+
+    if ((millis() - auxTime) > 1000)
+    {
+        Serial.println(valorObtido);
+        auxTime = millis();
+    }
+}
