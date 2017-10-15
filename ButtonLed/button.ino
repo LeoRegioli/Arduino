4972c370585bf2856ded74ffb29c1d7129958621
Leonardo Regioli <henrique_11@msn.com>
2017-10-15 16:56:20 -0200

    button

---
+++ ButtonLed/button.ino
@@ -0,0 +1,29 @@
+int led = 13,
+    button = 2,
+    auxTime = 0;
+
+byte state = 0, a = 0;
+
+void setup()
+{
+    pinMode(led, OUTPUT);
+    pinMode(button, INPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    a = digitalRead(button);
+        if (a == 0)
+        {
+
+            blink();
+            digitalWrite(led, state);
+            delay(200);
+        }
+}
+
+void blink()
+{
+    state = !state;
+}
